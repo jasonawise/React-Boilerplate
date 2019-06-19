70f99d9792b335d7c27bec0b2ef597a5af14834d
jasonwise <jasonandrewwise@gmail.com>
2019-06-19 16:08:35 -0400

    adding react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My React Boilerplate';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
